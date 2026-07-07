d2c14ff9a61ae4d792a9100c4d97d630594df511
gachomo caleb <gachomocaleb@CALEB-DESKTOP.localdomain>
2026-07-07 22:21:30 +0300

    submitting module assignment

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+require('datejs');
 
+function combineUsers(...arrays) {
+  const combinedobject = {
+    users: [],
+    
+  };
 
+  for (const array of arrays) {
+    combinedobject.users.push(...array);
+  }
+
+  combinedobject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedobject;
+
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
